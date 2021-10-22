d3f3f5b8a077611446f4bb0fc6579b64b77a0f5f
sekhparvez <sekhparvez@gmail.com>
2021-10-21 21:25:51 -0400

    added videos

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,17 +1,24 @@
 // store the link plus the API key in a variable
 const key = "uQG4jhIEHKHKm0qMKGcTHqUgAolr1GM0";
 const API = `https://api.nytimes.com/svc/topstories/v2/nyregion.json?api-key=${key}`;
+const storagePrefix = "nyt-autosave";
 
-console.log(API);
+import {setWithExpiry, getWithExpiry} from './modules/localeStorageHelpers.js'
 
 function getStories() {
-  fetch(API)
-    .then((response) => response.json())
-    .then((data) => showData(data.results));
+  const value = getWithExpiry(storagePrefix);
+  if (!value) {
+    console.log(" expired - fetching again ");
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => showData(data.results));
+  } else {
+    document.querySelector(".stories").innerHTML = value;
+  }
 }
 
 function showData(stories) {
-  var looped = stories
+  const looped = stories
     .map(
       (story) => `
     <div class="item">
@@ -29,8 +36,55 @@ function showData(stories) {
     .join("");
 
   document.querySelector(".stories").innerHTML = looped;
+  setWithExpiry(storagePrefix, looped, 1000 * 60);
 }
 
 if (document.querySelector(".home")) {
   getStories();
+}
+
+//nav script
+// var hamburger = document.querySelector("#pull");
+// var body = document.querySelector("body");
+
+document.addEventListener("click", clickHandlers);
+
+// function clickHandlers(event) {
+//   console.log(event.target);
+//   if (event.target.matches("#pull")) {
+//     document.querySelector("body").classList.toggle("show-nav");
+//     event.preventDefault();
+//   }
+//   // event.preventDefault();
+// }
+
+// //video
+
+function clickHandlers(event) {
+  if (event.target.matches("#pull")) {
+    document.querySelector("body").classList.toggle("show-nav");
+    event.preventDefault();
+  }
+  if (event.target.matches("#pull")) {
+      showMenu();
+      event.preventDefault();
+    }
+    if (event.target.matches(".content-video a")) {
+      videoSwitch(event);
+      event.preventDefault();
+    }
+  
+}
+
+function showMenu() {
+  document.querySelector("body").classList.toggle("show-nav");
+}
+
+function videoSwitch(event) {
+  const iFrame = document.querySelector("iframe");
+  const videoLinks = document.querySelectorAll(".content-video a");
+  videoLinks.forEach((videoLink) => videoLink.classList.remove("active"));
+  event.target.classList.add("active");
+  const videoToPlay = event.target.getAttribute("href");
+  iFrame.setAttribute("src", videoToPlay);
 }
